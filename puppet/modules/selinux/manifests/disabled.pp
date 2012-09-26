a6c2645cba2501151eaed643f0c3766b84c031c8
Ryan Doyle <ryan@doylenet.net>
2012-09-26 20:56:03 +1000

    Install selinux to get the sestatus command

--- puppet/modules/selinux/manifests/disabled.pp
+++ puppet/modules/selinux/manifests/disabled.pp
@@ -1,6 +1,10 @@
 class selinux::disabled {
+    # Needed for /usr/sbin/setenforce
+    package {'policycoreutils': ensure => installed }
+
     exec {'disable-selinux':
         command => "/usr/sbin/setenforce 0",
-        unless => "/usr/sbin/sestatus | /bin/grep 'Current mode:.*permissive'",
+        unless => "/usr/sbin/sestatus | /bin/grep -E 'Current mode:.*permissive|SELinux status:.*disabled'",
+        require => Package['policycoreutils'],
     }
 }
